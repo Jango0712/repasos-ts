bf7d8d2080374f901b714ec1e4b6e57708985ad8
Jango0712 <gomezmarchanj@gmail.com>
2023-07-15 01:20:46 +0100

    bucles while

---
+++ retos1/buclesWhile.ts
@@ -0,0 +1,27 @@
+const hasEven = (array: number[]): boolean  => {
+        let i:number = 0;
+        let encontrado = false;
+        while (i < array.length) {
+          if (array[i] % 2 === 0) {
+            encontrado = true;
+          }
+          i++;
+        }
+        return encontrado;
+    }
+
+    console.log(hasEven([2,3,5,2]))
+    
+    function startWithM(array: string[]): boolean {
+        let i = 0;
+        let todosConM = true;
+        while (i < array.length && todosConM) {
+          if (array[i][0] !== "m" && array[i][0] !== "M") {
+            todosConM = false;
+          }
+          i++;
+        }
+        return todosConM;
+    }
+
+    console.log(startWithM(["Jeo","mar"]))
